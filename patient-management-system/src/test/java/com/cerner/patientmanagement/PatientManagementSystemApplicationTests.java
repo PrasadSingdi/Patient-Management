6df3ca7203099b01e05fed3b41fad8020600f0b7
PrasadSingdi <prasad.singidi@gmail.com>
2023-07-06 12:41:17 +0530

    First commit

---
+++ patient-management-system/src/test/java/com/cerner/patientmanagement/PatientManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerner.patientmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
