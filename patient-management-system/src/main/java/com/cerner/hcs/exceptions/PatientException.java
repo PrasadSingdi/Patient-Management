eafb07a8861523005fca0cea9705aab74aeaa8c1
Prasad Singidi <38652780+PrasadSingdi@users.noreply.github.com>
2023-07-11 16:30:06 +0530

    Update PatientException.java

--- patient-management-system/src/main/java/com/cerner/hcs/exceptions/PatientException.java
+++ patient-management-system/src/main/java/com/cerner/hcs/exceptions/PatientException.java
@@ -1,12 +1,5 @@
-/**
- * 
- */
 package com.cerner.hcs.exceptions;
 
-/**
- * @author Mahesh
- *
- */
 public class PatientException extends Exception {
 
 	public PatientException(String message) {
